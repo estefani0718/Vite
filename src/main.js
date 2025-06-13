7af2fd30bb1a1fb0badd25f4b8a02ca2e09aba40
estefani0718 <eestefanisg@gmail.com>
2025-06-13 12:03:12 -0500

    mas ajustes

--- src/main.js
+++ src/main.js
@@ -3,10 +3,22 @@ import './style.css'
 let main = document.querySelector('.main')
 
 window.addEventListener('hashchange', async () => {
-  const hash = window.location.hash.slice('#');
+  const hash = window.location.hash.slice(1);
+  cargarVistas(hash)
+})
+window.addEventListener('DOMContentLoaded', async () => {
+  const hash = window.location.hash.slice(1);
+  if (hash) {
+    cargarVistas(hash)
+  } else {
+    const vacias="error"
+    main.innerHTML = vacias;
+  }
+})
 
-  const vistas = await fetch(`../src/view/categorias/${hash}.html`);
+async function cargarVistas(hash) {
+
+  const vistas = await fetch(`../src/view/${hash}/index.html`);
   const respuesta = await vistas.text();
   main.innerHTML = respuesta;
-  alert()
-})
+}
